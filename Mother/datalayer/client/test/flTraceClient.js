04dae3bbde330715b36b03f8c8a9be2f18ba7d35
nicofr83 <nicolas@cuvillier.net>
2014-07-29 10:38:56 +0400

    release 0.8.1

--- Mother/datalayer/client/test/flTraceClient.js
+++ Mother/datalayer/client/test/flTraceClient.js
@@ -6,15 +6,16 @@ var axDef= require('../ajaxNode.js');
 var ax = new axDef();
 var flMain=require('../flClient.js');
 var fl = new flMain(ax.callAjax);
+var apiSrv=process.env.FLAPISRV || 'localhost';
+var cliTrace=process.env.FLCLITRACE || 0;
 var fa = new fl.app();
 var fd = new fl.data();
 var fEnt= new fl.entity();
 var fField=new fl.field();
 var fRel=new fl.relation();
 
-fl.serverName('localhost');
-//fl.setTraceClient(2);
-//fl.setTraceServer(18)
+fl.serverName(apiSrv);
+fl.setTraceClient(cliTrace);
 fl.login({"username": "Nico", "password": "coLas"}, function (err, data){
 	if(err)
 		return console.log("I cannot connect to the service#1 !!");
