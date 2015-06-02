40399c44da13e2fe6474887eeacc60a8d8f2232d
kpatel1989 <katk.1989@gmail.com>
2015-06-02 23:56:50 +0530

    mailer bugs solved

--- mailer/js/View/PropertyPanels/StyleTab.js
+++ mailer/js/View/PropertyPanels/StyleTab.js
@@ -135,7 +135,7 @@ MailerTemplate.Views.StyleTab = Backbone.View.extend({
 	OnImageMarginChanged : function(e){
 		data = {
 			"property" : "imagePadding",
-			"value" : $(e.target).prop("checked") == "checked" ? "10px" : "0px"
+			"value" : $(e.currentTarget).prop("checked") ? "10px" : "0px"
 		}
 		this.trigger(MailerTemplate.Views.StyleTab.STYLE_PROPERTY_CHANGED,data);
 	},
